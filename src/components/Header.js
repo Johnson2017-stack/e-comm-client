b1966a38446c72c9ffb598c7148e6c2e12dc4338
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-08-25 20:57:22 -0700

    i bought in link to my header component, i put the reducer and stae provider components into their proper folder, i updated rducer to state with the basket, in product file i brought in usestatevalue and added the onclick to the button for the addbacket function i created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,18 @@ import React from 'react';
 import "./Header.css";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 
 const Header = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <>
        <div className="header">
-
-        <p id="LOGO">Johnson's MotorSports</p>
+        <Link to='/'>
+          <p id="LOGO">Johnson's MotorSports</p>
+        </Link>
 
     <div className="header__search">
       <input className="header__searchInput" type="text" />
@@ -30,14 +34,14 @@ const Header = () => {
           <span className="header__optionLineTwo">& Orders</span>
         </div>
 
-      
+        <Link to="/checkout">
         <div className="header__optionBasket">
           <AddShoppingCartIcon />
           <span className="header__optionLineTwo header__basketCount">
-            
+            {basket?.length}
           </span>
         </div>
-      
+        </Link>
     </div>
   </div>
     </>
