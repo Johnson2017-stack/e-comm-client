94f5b04627496b18a87f1fa5763061a0ebc2e2e4
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-08-29 16:21:58 -0700

    added stripe into my app to use wihin my payment component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import {Route, Routes} from "react-router-dom";
 import Home from './components/Home';
 import Header from './components/Header';
 import Checkout from "./components/Checkout";
+import Payment from "./components/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import  { Elements } from "@stripe/react-stripe-js"
+
+const promise = loadStripe("pk_test_51NkYniKc2t9beDQBoZqloJUVZXZiag8pnVKB6bnXh5Qh09kObJJwZManyl4BZSBdUSp2gGp36yevcSXzLdhLiftT00EuJwUSfs");
 
 
 const App = () => {
@@ -10,7 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={ <><Header /><Home /></>}/>
         <Route path="/checkout" element= {<><Header /><Checkout/></> } />
-        <Route path="/payment" element={<><Header /></>} />
+        <Route path="/payment" element={<><Header /><Elements stripe={promise}><Payment/></Elements></>} />
       </Routes>
     </div>
   );
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React  from 'react';
 import "./styles/Payment.css"
+// import axios from 'axios';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
+import {Link} from 'react-router-dom';
+import Checkout from './Checkout';
 
 const Payment = () => {
+    const[{ basket }, dispatch] = useStateValue();
+
+    // const [Name, setName] = useState("");
+    // const [Address, setAddress] = useState("");
+    // const [Phone, setPhone] = useState("");
+
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    // }
+
+    // axios.post()
+
   return (
-    <div>Payment</div>
+    <div className='payment'>
+        <div className='payment_container'>
+            <h1>Checkout (<Link path="/checkout" element={<Checkout/>}>{basket?.length} items</Link>)</h1>
+            <div className='payment_section'>
+                <div className='payment_title'>
+                    <h3>Delivery Address</h3>
+                </div>
+                <div className='payment_address'>
+                    <p>Valued Customer</p>
+                    <p>1234 johnsonMortorSports LANE</p>
+                    <p>Denver, Co</p>
+                    {/* <form>
+                        <input placeholder="Your Name here" type="text" name="Name" onChange={e => setName(e.target.value)}></input>
+                        <input placeholder='Your Address' type="text" name="Address" onChange={e => setAddress(e.target.value)}></input>
+                        <input placeholder='Your Phone Number' type="text" name="Phone" onChange={e => setPhone(e.target.value)}></input>
+                        <button>submit</button>
+                        
+                    </form> */}
+                </div>
+            </div>
+        <div className='payment_section'>
+            <div className='payment_title'>
+                <h3>Review items and delivery</h3>
+            </div>
+            <div className='payment_items'>
+                {basket.map(item => (
+                    <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                ))}
+            </div>
+        </div>
+        <div className='payment_section'>
+                <div className='payment_title'>
+                    <h3>Payment Method</h3>
+                </div>
+                <div className='payment_details'>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
